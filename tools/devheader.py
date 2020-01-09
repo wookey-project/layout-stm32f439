0efb1fdd6394a554be524f7e4c341308fcc97cd4
Philippe Thierry <phil@reseau-libre.net>
2020-01-09 16:56:20 +0100

    [update] add devid.h autogenerated header

--- tools/devheader.py
+++ tools/devheader.py
@@ -133,75 +133,85 @@ def generate_c():
             devinfofile.write(c_definition);
             devinfofile.write("#endif/*!DEVINFO_H_*/\n");
 
-    for device in data:
-        dev = device["name"];
-        device_c_name = dev.replace("-", "_");
-        if device["size"] == "0" and device["type"] == "block":
-            # we do not generate headers for unmappable block device (e.g. DMA)
-            continue;
-        devfilename = device_c_name + ".h";
-        devheadername = device_c_name.upper() + "_H_";
+    with open(os.path.join(outdir, 'devlist.h'), "w") as devlistfile:
+        # preparing for devlist file (file listing all devices unique identifier)
+        devlistfile.write(c_header);
+        devlistfile.write("#ifndef DEVLIST_H_\n");
+        devlistfile.write("# define DEVLIST_H_\n\n");
 
-        with open(os.path.join(outdir, devfilename), "w") as devfile:
-            # header (license)
-            devfile.write(c_header);
-            # preprocessing and inclusion
-            devfile.write("#ifndef %s\n" % devheadername);
-            devfile.write("# define %s\n" % devheadername);
-            devfile.write("\n#include \"generated/devinfo.h\"\n\n");
+        for device in data:
+            dev = device["name"];
+            device_c_name = dev.replace("-", "_");
+            if device["size"] == "0" and device["type"] == "block":
+                # we do not generate headers for unmappable block device (e.g. DMA)
+                continue;
+            devfilename = device_c_name + ".h";
+            devheadername = device_c_name.upper() + "_H_";
 
-            devfile.write("# define %s_BASE %s\n" % (device_c_name.upper(), device["address"]));
+            with open(os.path.join(outdir, devfilename), "w") as devfile:
+                # header (license)
+                devfile.write(c_header);
+                # preprocessing and inclusion
+                devfile.write("#ifndef %s\n" % devheadername);
+                devfile.write("# define %s\n" % devheadername);
+                devfile.write("\n#include \"generated/devinfo.h\"\n\n");
 
-            if device["type"] == "block":
-                # generating defines for IRQ values
-                if 'irqs' in device:
-                    irqs = device["irqs"];
-                    for irq in irqs:
-                        if irq["value"] != 0:
-                            devfile.write("#define %s %s\n" % (irq["name"], irq["value"]));
+                devfile.write("# define %s_BASE %s\n" % (device_c_name.upper(), device["address"]));
 
-            if 'gpios' in device:
-                gpios = device["gpios"];
-                devfile.write("/* naming indexes in structure gpios[] table */\n");
-                for index, gpio in enumerate(gpios):
-                    devfile.write("#define %s %d\n" % (gpio["name"], index));
+                if device["type"] == "block":
+                    # generating defines for IRQ values
+                    if 'irqs' in device:
+                        irqs = device["irqs"];
+                        for irq in irqs:
+                            if irq["value"] != 0:
+                                devfile.write("#define %s %s\n" % (irq["name"], irq["value"]));
 
-            if 'dmas' in device:
-                dmas = device["dmas"];
-                devfile.write("#define %s %s\n" % (dmas["controler_name"], dmas["controler_id"]));
-                devfile.write("/* naming indexes in structure dmas[] table */\n");
-                dma_info_list = dmas["dma"];
-                for dma_info in dma_info_list:
-                    devfile.write("#define %s %s\n" % (dma_info["name"], dma_info["value"]));
+                if 'gpios' in device:
+                    gpios = device["gpios"];
+                    devfile.write("/* naming indexes in structure gpios[] table */\n");
+                    for index, gpio in enumerate(gpios):
+                        devfile.write("#define %s %d\n" % (gpio["name"], index));
 
-            # global variable declaration
-            devfile.write("\nstatic const struct user_driver_device_infos %s_dev_infos = {\n" % device_c_name);
-            # device address
-            devfile.write("    .address = %s,\n" % device["address"]);
-            # device size
-            devfile.write("    .size    = %s,\n" % device["size"]);
-            devfile.write("    .id      = %d,\n" % device_id);
-            device_id += 1;
+                if 'dmas' in device:
+                    dmas = device["dmas"];
+                    devfile.write("#define %s %s\n" % (dmas["controler_name"], dmas["controler_id"]));
+                    devfile.write("/* naming indexes in structure dmas[] table */\n");
+                    dma_info_list = dmas["dma"];
+                    for dma_info in dma_info_list:
+                        devfile.write("#define %s %s\n" % (dma_info["name"], dma_info["value"]));
 
-            # device gpios
-            devfile.write("    .gpios = {\n");
-            if 'gpios' in device:
-                gpios = device["gpios"];
-                for gpio in gpios[0:]:
-                    devfile.write("      { %s, %s },\n" % (gpio["port"], gpio["pin"]));
-                if len(gpios) < max_gpio_num:
-                    for i in range(len(gpios), max_gpio_num):
+                # global variable declaration
+                devfile.write("\nstatic const struct user_driver_device_infos %s_dev_infos = {\n" % device_c_name);
+                # device address
+                devfile.write("    .address = %s,\n" % device["address"]);
+                # device size
+                devfile.write("    .size    = %s,\n" % device["size"]);
+                devfile.write("    .id      = %d,\n" % device_id);
+
+                devlistfile.write("# define %s_ID %s\n" % (device_c_name.upper(), device_id));
+
+                device_id += 1;
+
+                # device gpios
+                devfile.write("    .gpios = {\n");
+                if 'gpios' in device:
+                    gpios = device["gpios"];
+                    for gpio in gpios[0:]:
+                        devfile.write("      { %s, %s },\n" % (gpio["port"], gpio["pin"]));
+                    if len(gpios) < max_gpio_num:
+                        for i in range(len(gpios), max_gpio_num):
+                            devfile.write("      { 0, 0 },\n");
+
+                else:
+                    for i in range(1,13):
                         devfile.write("      { 0, 0 },\n");
 
-            else:
-                for i in range(1,13):
-                    devfile.write("      { 0, 0 },\n");
+                devfile.write("    }\n");
+                devfile.write("};\n");
 
-            devfile.write("    }\n");
-            devfile.write("};\n");
+                # closing preprocessing
+                devfile.write(c_footer);
 
-            # closing preprocessing
-            devfile.write(c_footer);
-
+        devlistfile.write(c_footer);
 
 generate_c();
